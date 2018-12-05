dd920eb02986dc576a494d675e967a638b58ccf9
Beta-guy <45407477+Beta-guy@users.noreply.github.com>
2018-12-05 13:03:07 -0700

    Add files via upload
    added a loop and made exit faster (less annoying)

--- calc.py
+++ calc.py
@@ -2,23 +2,21 @@ import calclib
 print("=" * 20)
 print("| Stephanie's Calc |")
 print("=" * 20)
-
-num1 = float(input("Input 1st number: "))
-
-op = input("What operator do you want to use (add, minus, divide, or multiply): ")
-
-num2 = float(input("Input 2nd number: "))
-if op.lower() == "add" or op == "+":
-    print(calclib.add(num1, num2))
-elif op.lower() == "minus" or op.lower() == "subtract" or op == "-":
-    print(calclib.minus(num1, num2))
-elif op.lower() == "divide" or op == "/":
-    print(calclib.divide(num1, num2))
-elif op.lower() == "multiply" or op.lower() == "times" or op == "*" or op == "x":
-    print(calclib.multiply(num1, num2))
-elif op.lower() == "exit":
-    quit(0)
-elif op.lower() == "power" or op.lower() == "^":
-    print(calclib.power_of(num1, num2))
-else:
-    print("Invalid Operator")
+while True:
+    num1 = float(input("\nInput 1st number: "))
+    op = input("\nWhat operator do you want to use (add, minus, divide, or multiply): ")
+    if op.lower() == "exit":
+        quit(0)
+    num2 = float(input("\nInput 2nd number: "))
+    if op.lower() == "add" or op == "+":
+        print(calclib.add(num1, num2))
+    elif op.lower() == "minus" or op.lower() == "subtract" or op == "-":
+        print(calclib.minus(num1, num2))
+    elif op.lower() == "divide" or op == "/":
+        print(calclib.divide(num1, num2))
+    elif op.lower() == "multiply" or op.lower() == "times" or op == "*" or op == "x":
+        print(calclib.multiply(num1, num2))
+    elif op.lower() == "power" or op.lower() == "^":
+        print(calclib.power_of(num1, num2))
+    else:
+        print("Invalid Operator")
